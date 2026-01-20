a0b09042c198cb643fb9359eb40b0fae1f527709
ElecGeek <95126843+ElecGeek@users.noreply.github.com>
2026-01-20 10:59:38 +0100

    Rename the input files into ...json

--- cpp_version/params_input_json.cpp
+++ cpp_version/params_input_json.cpp
@@ -1,21 +1,21 @@
-#include "params_input_atpos.hxx"
+#include "params_input_json.hxx"
 #include <cmath>
 
-atpos_event::atpos_event():
+json_event::json_event():
   action (nullopt),
   metadata (nullopt)
 {}
-atpos_event::atpos_event(const string&at_key,const string&at_value):
+json_event::json_event(const string&at_key,const string&at_value):
   action (nullopt)
 {
-  optional<atpos_event_metadata> metadata;
+  optional<json_event_metadata> metadata;
   metadata->md_key = at_key;
   metadata->md_val = at_value;
   this->metadata = metadata;
 }
 
 
-atposs_bytes_stream::atposs_bytes_stream(ostream&os,
+jsons_bytes_stream::jsons_bytes_stream(ostream&os,
 										 const bool&with_time_stamp,
 										 input_params_base::clearing_t&clearing ):
   state( state_end ),
@@ -27,7 +27,7 @@ atposs_bytes_stream::atposs_bytes_stream(ostream&os,
   with_time_stamp( with_time_stamp )
 {}
 
-bool atposs_bytes_stream::get_event(istream&i_stm)
+bool jsons_bytes_stream::get_event(istream&i_stm)
 {
   // Since it is a stream char iterator, this function checks for the eof before each read
   // The file might be finish or the stream is temporarily starving.
@@ -84,7 +84,7 @@ bool atposs_bytes_stream::get_event(istream&i_stm)
 	}
 }
 
-atposs_bytes_datagram_test::atposs_bytes_datagram_test(ostream&os,
+jsons_bytes_datagram_test::jsons_bytes_datagram_test(ostream&os,
 										 const bool&with_time_stamp,
 										 input_params_base::clearing_t&clearing ):
   info_out_str(os),
@@ -94,8 +94,8 @@ atposs_bytes_datagram_test::atposs_bytes_datagram_test(ostream&os,
 	info_out_str << "With time stamps is required, since this is a test module, it is omitted" << endl;
 }
 
-bool atposs_bytes_datagram_test::get_event(deque<atpos_event>::const_iterator&curr,
-										   deque<atpos_event>::const_iterator&end)
+bool jsons_bytes_datagram_test::get_event(deque<json_event>::const_iterator&curr,
+										   deque<json_event>::const_iterator&end)
 {
   if( curr != end )
 	{
@@ -111,8 +111,8 @@ bool atposs_bytes_datagram_test::get_event(deque<atpos_event>::const_iterator&cu
 
 
 
-input_params_atposs_2_action::input_params_atposs_2_action( ostream&out_info_str,
-															const atpos_event&the_event,
+input_params_jsons_2_action::input_params_jsons_2_action( ostream&out_info_str,
+															const json_event&the_event,
 															input_params_base::clearing_t&clearing,
 															input_event::status_t&status):
   info_out_str(info_out_str),
@@ -126,32 +126,32 @@ input_params_atposs_2_action::input_params_atposs_2_action( ostream&out_info_str
 		  return 0;}},
 	}),
 
-  atposs_list({
+  jsons_list({
 	  {P_abort,"NQ"},{P_abort,"nq"}})
 {}
 
 
 
-void input_params_atposs_2_action::atposs_2_action_run(vector<signals_param_action>&actions)
+void input_params_jsons_2_action::jsons_2_action_run(vector<signals_param_action>&actions)
 {
   signals_param_action action;
-  decltype(mrf)::const_iterator atposs_read_funcs_iter;
-  multimap<short,string>::const_iterator atposs_list_iter;
+  decltype(mrf)::const_iterator jsons_read_funcs_iter;
+  multimap<short,string>::const_iterator jsons_list_iter;
   string return_err_str;
 
-  /*  if( the_event.atpos.compare( "bh" ) == 0 || the_event.atpos.compare( "bi" ) == 0|| the_event.atpos.compare( "bf" ) == 0)
+  /*  if( the_event.json.compare( "bh" ) == 0 || the_event.json.compare( "bi" ) == 0|| the_event.json.compare( "bf" ) == 0)
  	{
-	  cout << "Atpos to find: " << the_event.atpos << "\t";
+	  cout << "Json to find: " << the_event.json << "\t";
 	  cout << the_event.TS_left << "." << the_event.TS_right << "\t" << the_event.channel << "\t";
 	  cout << the_event.value_left << "." << the_event.value_right << endl;
 	  }*/
 
-  atposs_read_funcs_iter = mrf.end(); //mrf.find(the_event.atpos);
-  if ( atposs_read_funcs_iter != mrf.end() )
+  jsons_read_funcs_iter = mrf.end(); //mrf.find(the_event.json);
+  if ( jsons_read_funcs_iter != mrf.end() )
 	{
 	  unsigned long value;
 	  signals_param_action::action_list act;
-	  return_err_str = atposs_read_funcs_iter->second( value, act );
+	  return_err_str = jsons_read_funcs_iter->second( value, act );
 	  if ( return_err_str.empty() )
 		{
 		  action.channel_id = 0; // we should decide how to pass the channel number     current_channel;
@@ -168,27 +168,27 @@ void input_params_atposs_2_action::atposs_2_action_run(vector<signals_param_acti
 		  actions.push_back( action );
 		}
 	  else
-		//		cout << return_err_str << ", channel: " << the_event.channel << ", atpos: " << the_event.atpos << ", unknown channel" << endl;
+		//		cout << return_err_str << ", channel: " << the_event.channel << ", json: " << the_event.json << ", unknown channel" << endl;
 	    cout  << "ERROR" << endl;
 	}else
 	{
 	  // Not found, 3 reasons
-	  //   * atpos handled in the old version
-	  //   * atpos of the extended commands ( store, retrieve etc... NOT YET )
+	  //   * json handled in the old version
+	  //   * json of the extended commands ( store, retrieve etc... NOT YET )
 	  //   * really not found
 	  // The store retrieve is on hold as a decision has to be taken:
 	  //   * does it move to the low level parameters ( recording parameter action structures )
 	  //   * should it handled as file pointer. That means it is available in seekable inputs only
-	  if ( atposs_list_iter != atposs_list.end() )
+	  if ( jsons_list_iter != jsons_list.end() )
 		{
 		  unsigned long value;
 		  unsigned char val_c;
-		  atposs_list_iter = find_if( atposs_list.begin(), atposs_list.end(),
+		  jsons_list_iter = find_if( jsons_list.begin(), jsons_list.end(),
 									  [&](multimap<short,string>::const_reference a) -> bool {
-										return a.second == string() /*the_event.atpos*/;
+										return a.second == string() /*the_event.json*/;
 									  });
 		  /* don't know now how to abort
-			 switch( atposs_list_iter->first )
+			 switch( jsons_list_iter->first )
 			{
 			case P_abort:
 			  ipm2a_status = input_event::end_track;
@@ -202,30 +202,30 @@ void input_params_atposs_2_action::atposs_2_action_run(vector<signals_param_acti
 			  actions.push_back( action );
 			}
 		  else
-			//	cout << return_err_str << ", channel: " << the_event.channel << ", atpos: " << the_event.atpos << endl;
+			//	cout << return_err_str << ", channel: " << the_event.channel << ", json: " << the_event.json << endl;
 			cout << "ERROR" << endl;
 		}
 	  else
-		return_err_str = "Unknown atpos. ";
+		return_err_str = "Unknown json. ";
 	}
 }
 
-input_params_atposs_byte_stream::input_params_atposs_byte_stream(ostream&, istream&i_stm, const bool&with_time_stamp):
+input_params_jsons_byte_stream::input_params_jsons_byte_stream(ostream&, istream&i_stm, const bool&with_time_stamp):
   i_stm( i_stm ),
   info_out_str_stream( ioss ),
-  atposs_bytes_stream( info_out_stream, with_time_stamp, ((input_params_base*)this)->clearing ),
-  input_params_atposs_2_action( info_out_stream, *((atpos_event*)this), ((input_params_base*)this)->clearing, status )
+  jsons_bytes_stream( info_out_stream, with_time_stamp, ((input_params_base*)this)->clearing ),
+  input_params_jsons_2_action( info_out_stream, *((json_event*)this), ((input_params_base*)this)->clearing, status )
 {
   ioss.reserve( 100 );
 }
 
 
-void input_params_atposs_byte_stream::import_next_event(vector<signals_param_action>&actions)
+void input_params_jsons_byte_stream::import_next_event(vector<signals_param_action>&actions)
 {
-  atposs_2_action_run( actions );
+  jsons_2_action_run( actions );
 }
 
-unsigned long input_params_atposs_byte_stream::check_next_time_stamp()
+unsigned long input_params_jsons_byte_stream::check_next_time_stamp()
 {
   if ( get_event( i_stm ) )
 	{
@@ -240,17 +240,17 @@ unsigned long input_params_atposs_byte_stream::check_next_time_stamp()
 	// Input is "starving" nothing has been received or is not complete
 	return 0xffffffff;
 }
-bool input_params_atposs_byte_stream::eot(void) const
+bool input_params_jsons_byte_stream::eot(void) const
 {
   return false;
 }
-bool input_params_atposs_byte_stream::is_ready(void)
+bool input_params_jsons_byte_stream::is_ready(void)
 {
   return true;
 }
 
 
-ostream&operator<<(ostream&the_out,const input_params_atposs_byte_stream&)
+ostream&operator<<(ostream&the_out,const input_params_jsons_byte_stream&)
 {
   
 
@@ -258,10 +258,10 @@ ostream&operator<<(ostream&the_out,const input_params_atposs_byte_stream&)
 }
 
 
-input_params_atposs_file::input_params_atposs_file( ostream&, ifstream&input_stream, const unsigned short&loops_counter ):
+input_params_jsons_file::input_params_jsons_file( ostream&, ifstream&input_stream, const unsigned short&loops_counter ):
   info_out_str_stream( ioss ),
-  input_params_atposs_2_action( info_out_str_stream, *((atpos_event*)this), ((input_params_base*)this)->clearing, status ),
-  atposs_bytes_stream( info_out_str_stream, true, ((input_params_base*)this)->clearing ),
+  input_params_jsons_2_action( info_out_str_stream, *((json_event*)this), ((input_params_base*)this)->clearing, status ),
+  jsons_bytes_stream( info_out_str_stream, true, ((input_params_base*)this)->clearing ),
   loops_counter( loops_counter ),
   if_stm( move( input_stream ))
 {
@@ -272,25 +272,25 @@ input_params_atposs_file::input_params_atposs_file( ostream&, ifstream&input_str
 	  clearing = c_file_err;
 	}
 }
-input_params_atposs_file::~input_params_atposs_file()
+input_params_jsons_file::~input_params_jsons_file()
 {
   // TODO check here if the length is the declared length. if not report a warning
   if_stm.close();
 }
 
-void input_params_atposs_file::import_next_event(vector<signals_param_action>&actions)
+void input_params_jsons_file::import_next_event(vector<signals_param_action>&actions)
 {
   // cout << "TS cumul: " << dec << cumul_time_stamp / 10 << '\t';
   // cout << endl;
 
-  atposs_2_action_run( actions );
+  jsons_2_action_run( actions );
   // Reset the fields
   // The structure should change. For now initialize one by one
   /*  TS_left.clear();
   TS_right.clear();
   TS_unit.clear();
   channel.clear();
-  atpos.clear();
+  json.clear();
   value_left.clear();
   value_right.clear();
   value_unit.clear();*/
@@ -302,7 +302,7 @@ void input_params_atposs_file::import_next_event(vector<signals_param_action>&ac
 	}
 }
 
-unsigned long input_params_atposs_file::check_next_time_stamp()
+unsigned long input_params_jsons_file::check_next_time_stamp()
 {
   if ( get_event( if_stm ) )
 	{
@@ -318,11 +318,11 @@ unsigned long input_params_atposs_file::check_next_time_stamp()
 	// Input is "starving" nothing has been received or is not complete
 	return 0xffffffff;
 }
-bool input_params_atposs_file::eot()const
+bool input_params_jsons_file::eot()const
 {
   return status == end_track;
 }
-bool input_params_atposs_file::is_ready()
+bool input_params_jsons_file::is_ready()
 {
   if ( samples_per_TS_unity != 5 )
 	cout << "Internal ERROR: samples_per_TS_unity not 5" << endl;
@@ -334,7 +334,7 @@ bool input_params_atposs_file::is_ready()
  * It computes the loop counter and tell if it is over
  * \return true after the last run
  */
-bool input_params_atposs_file::exec_loops(){
+bool input_params_jsons_file::exec_loops(){
   cout << "Checking loop" << endl;
   if( loops_counter > 0 )
 	{
@@ -346,54 +346,54 @@ bool input_params_atposs_file::exec_loops(){
 	return false;
 }
 
-input_params_atposs_hardcoded::input_params_atposs_hardcoded(ostream&):
+input_params_jsons_hardcoded::input_params_jsons_hardcoded(ostream&):
   info_out_str_stream( ioss ),
-  atposs_bytes_datagram_test( info_out_stream, false, ((input_params_base*)this)->clearing ),
-  input_params_atposs_2_action( info_out_stream, *((atpos_event*)this), ((input_params_base*)this)->clearing, status )
-  /*  the_list({{atpos_event("0","0","s","1","nn","","","/")},
-		{atpos_event("","0","s","1","XX","22","5","/")},
-		{atpos_event("0","","","5","aa","1","20000","%")},
-		{atpos_event("1","2","","6","aa","100","4","%")},
-		{atpos_event("1","2","s","4","ba","100","0","%")},
-		{atpos_event("0","0","s","3","ba","90","0","")},
-		{atpos_event("1","2","s","4","ba","100","0","%")},
-		{atpos_event("1","2","s","7","bm","90","0","")},
-		{atpos_event("1","21","s","8","am","91","0","/")},
-		{atpos_event("1","21","s","9","am","1","1","/")},
-		{atpos_event("1","21","s","1","oo","22","5","/")},
-		{atpos_event("1","21","s","all","nn","466666666666666665","0","/")},
-		{atpos_event("1","21","s","all","bo","45","500","/")},
-		{atpos_event("1","21","s","3","ao","90","0","/")},
-	    {atpos_event("1","2","s","ALL","op","0","5","")},
-        {atpos_event("1","2","s","0","bp","45","500","/")},
-	    {atpos_event("1","2","s","1","ap","90","0","/")},
-		{atpos_event("1","2","s","20","of","0","20000","%")},
-		{atpos_event("1","2","s","21","of","100","4","")},
-		{atpos_event("1","2","s","22","of","1000","0","m")},
-		{atpos_event("1","2","s","23","of","90","0","mhz")},
-		{atpos_event("1","2","s","24","of","1","666","msec")},
-		{atpos_event("1","2","s","25","of","","00166","sec")},
-		{atpos_event("1","2","s","26","of","","600","khz")},
-		{atpos_event("1","2","s","27","of","1","600","hz")},
-		{atpos_event("1","2","s","30","os","0","0166","sec")},
-        {atpos_event("1","2","s","31","os","0","100","khz")},
-		{atpos_event("1","2","s","40","af","1","2000","%")},
-		{atpos_event("1","2","s","41","bf","100","04","")},
-		{atpos_event("1200","0","us","42","af","100","0","m")},
-		{atpos_event("1","2","","43","bf","90","0","mhz")},
-		{atpos_event("1","2","","44","af","1","066","msec")},
-		{atpos_event("1","2","","45","bf","0","0","sec")},
-		{atpos_event("1","2","s","50","bh","0","00166","sec")},
-		{atpos_event("1","2","s","51","bh","0","600","khz")},
-		{atpos_event("1","2","s","52","bi","0","0166","sec")},
-        {atpos_event("1","2","s","53","bi","10","0","s")}}
+  jsons_bytes_datagram_test( info_out_stream, false, ((input_params_base*)this)->clearing ),
+  input_params_jsons_2_action( info_out_stream, *((json_event*)this), ((input_params_base*)this)->clearing, status )
+  /*  the_list({{json_event("0","0","s","1","nn","","","/")},
+		{json_event("","0","s","1","XX","22","5","/")},
+		{json_event("0","","","5","aa","1","20000","%")},
+		{json_event("1","2","","6","aa","100","4","%")},
+		{json_event("1","2","s","4","ba","100","0","%")},
+		{json_event("0","0","s","3","ba","90","0","")},
+		{json_event("1","2","s","4","ba","100","0","%")},
+		{json_event("1","2","s","7","bm","90","0","")},
+		{json_event("1","21","s","8","am","91","0","/")},
+		{json_event("1","21","s","9","am","1","1","/")},
+		{json_event("1","21","s","1","oo","22","5","/")},
+		{json_event("1","21","s","all","nn","466666666666666665","0","/")},
+		{json_event("1","21","s","all","bo","45","500","/")},
+		{json_event("1","21","s","3","ao","90","0","/")},
+	    {json_event("1","2","s","ALL","op","0","5","")},
+        {json_event("1","2","s","0","bp","45","500","/")},
+	    {json_event("1","2","s","1","ap","90","0","/")},
+		{json_event("1","2","s","20","of","0","20000","%")},
+		{json_event("1","2","s","21","of","100","4","")},
+		{json_event("1","2","s","22","of","1000","0","m")},
+		{json_event("1","2","s","23","of","90","0","mhz")},
+		{json_event("1","2","s","24","of","1","666","msec")},
+		{json_event("1","2","s","25","of","","00166","sec")},
+		{json_event("1","2","s","26","of","","600","khz")},
+		{json_event("1","2","s","27","of","1","600","hz")},
+		{json_event("1","2","s","30","os","0","0166","sec")},
+        {json_event("1","2","s","31","os","0","100","khz")},
+		{json_event("1","2","s","40","af","1","2000","%")},
+		{json_event("1","2","s","41","bf","100","04","")},
+		{json_event("1200","0","us","42","af","100","0","m")},
+		{json_event("1","2","","43","bf","90","0","mhz")},
+		{json_event("1","2","","44","af","1","066","msec")},
+		{json_event("1","2","","45","bf","0","0","sec")},
+		{json_event("1","2","s","50","bh","0","00166","sec")},
+		{json_event("1","2","s","51","bh","0","600","khz")},
+		{json_event("1","2","s","52","bi","0","0166","sec")},
+        {json_event("1","2","s","53","bi","10","0","s")}}
 		)*/
 {
   the_list_iter = the_list.begin();
   the_list_end = the_list.end();
   ioss.reserve( 100 );
 }
-unsigned long input_params_atposs_hardcoded::check_next_time_stamp()
+unsigned long input_params_jsons_hardcoded::check_next_time_stamp()
 {
   string return_err_str;
   if ( get_event( the_list_iter, the_list_end ) )
@@ -409,24 +409,24 @@ unsigned long input_params_atposs_hardcoded::check_next_time_stamp()
 	// Input is "starving", that should not be happened here
 	return 0xffffffff;
 }
-void input_params_atposs_hardcoded::import_next_event(vector<signals_param_action>&actions)
+void input_params_jsons_hardcoded::import_next_event(vector<signals_param_action>&actions)
 {
   //cout << "TS cumul: " << dec << cumul_time_stamp / 10 << '\t';
   //cout << (unsigned short)key ;
 
-  atposs_2_action_run( actions );
+  jsons_2_action_run( actions );
 }
-bool input_params_atposs_hardcoded::eot(void) const
+bool input_params_jsons_hardcoded::eot(void) const
 {
   return status == end_track;
 }
-bool input_params_atposs_hardcoded::is_ready(void)
+bool input_params_jsons_hardcoded::is_ready(void)
 {
   if ( samples_per_TS_unity != 5 )
 	cout << "Internal ERROR: samples_per_TS_unity not 5" << endl;
   return true;
 }
-bool input_params_atposs_hardcoded::exec_loops()
+bool input_params_jsons_hardcoded::exec_loops()
 {
   return false;
 }
--- cpp_version/params_input_json.hxx
+++ cpp_version/params_input_json.hxx
@@ -9,28 +9,28 @@
 #include <optional>
 using namespace std;
 
-#ifndef __PARAMS_INPUT_ATPOSS__
-#define __PARAMS_INPUT_ATPOSS__
+#ifndef __PARAMS_INPUT_JSONS__
+#define __PARAMS_INPUT_JSONS__
 
 #include "parameters.hxx"
 
-class atpos_event_action {
+class json_event_action {
   unsigned long rel_timestamp;
   unsigned char level;
 };
-struct atpos_event_metadata {
+struct json_event_metadata {
   string md_key;
   string md_val;
 };
-struct atpos_event {
-  atpos_event(void);
-  atpos_event(const unsigned long&,const unsigned char);
-  atpos_event(const string&,const string&);
-  optional<atpos_event_action> action;
-  optional<atpos_event_metadata> metadata;
+struct json_event {
+  json_event(void);
+  json_event(const unsigned long&,const unsigned char);
+  json_event(const string&,const string&);
+  optional<json_event_action> action;
+  optional<json_event_metadata> metadata;
 };
 
-class atposs_bytes_stream : public atpos_event, public input_event {
+class jsons_bytes_stream : public json_event, public input_event {
   // just copied and pasted, have to be enriched by the line parsing
   enum state_t{ state_ts, state_code, state_key, state_val, state_string, state_end } state;
   enum { blk_start=0, blk_in_channel=1, blk_in_act_list=2, blk_in_action=4} block_state;
@@ -50,27 +50,27 @@ protected:
   unsigned short current_channel;
   const bool with_time_stamp;
 public:
-  atposs_bytes_stream(void)=delete;
-  atposs_bytes_stream(ostream&,const bool&with_time_stamp, input_params_base::clearing_t&clearing );
+  jsons_bytes_stream(void)=delete;
+  jsons_bytes_stream(ostream&,const bool&with_time_stamp, input_params_base::clearing_t&clearing );
   bool get_event(istream&);
   bool eot(void) const;
-  // For debug purposes, sends the content of a atpos message
-  friend ostream&operator<<( ostream&, const atposs_bytes_stream& );
+  // For debug purposes, sends the content of a json message
+  friend ostream&operator<<( ostream&, const jsons_bytes_stream& );
 };
-class atposs_bytes_datagram_test : public atpos_event, public input_event {
+class jsons_bytes_datagram_test : public json_event, public input_event {
   ostream&info_out_str;
   input_params_base::clearing_t&mbs_clearing;
 public:
-  atposs_bytes_datagram_test(void)=delete;
-  atposs_bytes_datagram_test(ostream&,const bool&with_time_stamp, input_params_base::clearing_t&clearing );
-  bool get_event(deque<atpos_event>::const_iterator&curr,
-				 deque<atpos_event>::const_iterator&end);
+  jsons_bytes_datagram_test(void)=delete;
+  jsons_bytes_datagram_test(ostream&,const bool&with_time_stamp, input_params_base::clearing_t&clearing );
+  bool get_event(deque<json_event>::const_iterator&curr,
+				 deque<json_event>::const_iterator&end);
   bool eot(void) const;
 };
-class input_params_atposs_2_action
+class input_params_jsons_2_action
 {
   ostream&info_out_str;
-  enum params_input_atposs_extended { P_abort = 100
+  enum params_input_jsons_extended { P_abort = 100
   };
 protected:
   /* \brief Converts channel data into channel code
@@ -86,10 +86,10 @@ protected:
   optional<unsigned long> timestamp_value;
 private:
   unsigned long get_value( const unsigned char&exponent_size, const unsigned char&exponent_const )const;
-  const atpos_event&the_event;
+  const json_event&the_event;
   input_params_base::clearing_t&ipm2a_clearing;
   input_event::status_t&ipm2a_status;
-  const multimap< short, string >atposs_list;
+  const multimap< short, string >jsons_list;
   // Not that readable, but it avoid excessive indentation in the constructor
   const map< string, function< string(unsigned long&,signals_param_action::action_list&)> >mrf;
   //constexpr unsigned long long str2ll(const char*str, int h = 0)
@@ -100,12 +100,12 @@ private:
   //	  return str2ll( str, h + 1 ) * 256 | (unsigned long long) str[ h ];
   //}
 public:
-  input_params_atposs_2_action(void)=delete;
-  input_params_atposs_2_action(ostream&info_out_str,
-							   const atpos_event&,
+  input_params_jsons_2_action(void)=delete;
+  input_params_jsons_2_action(ostream&info_out_str,
+							   const json_event&,
 							  input_params_base::clearing_t&clearing,
 							  input_event::status_t&status);
-  void atposs_2_action_run(vector<signals_param_action>&actions_list);
+  void jsons_2_action_run(vector<signals_param_action>&actions_list);
 };
 
 
@@ -120,19 +120,19 @@ public:
  * This should go into a template, but let's wait more interfaces to write a generic template,
  *   based on the 7 network OSI layer style
  */
-class input_params_atposs_byte_stream : public input_params_base, public atposs_bytes_stream, public input_params_atposs_2_action {
+class input_params_jsons_byte_stream : public input_params_base, public jsons_bytes_stream, public input_params_jsons_2_action {
   istream& i_stm;
   // string for the ostringstream, in order to reserve size
   string ioss;
   ostringstream info_out_str_stream;  
  public:
-  input_params_atposs_byte_stream(void)=delete;
-  input_params_atposs_byte_stream(ostream&,istream&,const bool&);
+  input_params_jsons_byte_stream(void)=delete;
+  input_params_jsons_byte_stream(ostream&,istream&,const bool&);
   void import_next_event(vector<signals_param_action>&actions);
   unsigned long check_next_time_stamp(void);
   bool eot(void) const;
   bool is_ready(void);
-  friend ostream&operator<<(ostream&,const input_params_atposs_byte_stream&);
+  friend ostream&operator<<(ostream&,const input_params_jsons_byte_stream&);
 };
 /** \brief Bundle class byte file
  *
@@ -145,16 +145,16 @@ class input_params_atposs_byte_stream : public input_params_base, public atposs_
  * This should go into a template, but let's wait more interfaces to write a generic template,
  *   based on the 7 network OSI layer style
  */
-class input_params_atposs_file : public input_params_base, public atposs_bytes_stream, public input_params_atposs_2_action{
+class input_params_jsons_file : public input_params_base, public jsons_bytes_stream, public input_params_jsons_2_action{
   ifstream if_stm;
   // string for the ostringstream, in order to reserve size
   string ioss;
   ostringstream info_out_str_stream;  
   unsigned short loops_counter;
  public:
-  input_params_atposs_file(void)=delete;
-  input_params_atposs_file(ostream&,ifstream&input_stream,const unsigned short&loops_counter);
-  ~input_params_atposs_file(void);
+  input_params_jsons_file(void)=delete;
+  input_params_jsons_file(ostream&,ifstream&input_stream,const unsigned short&loops_counter);
+  ~input_params_jsons_file(void);
   void import_next_event(vector<signals_param_action>&actions);
   unsigned long check_next_time_stamp(void);
   bool eot(void) const;
@@ -173,16 +173,16 @@ class input_params_atposs_file : public input_params_base, public atposs_bytes_s
  * This should go into a template, but let's wait more interfaces to write a generic template,
  *   based on the 7 network OSI layer style
  */
-class input_params_atposs_hardcoded : public input_params_base, public atposs_bytes_datagram_test, public input_params_atposs_2_action{
-  const deque<atpos_event>the_list;
-  deque<atpos_event>::const_iterator the_list_iter;
-  deque<atpos_event>::const_iterator the_list_end;
+class input_params_jsons_hardcoded : public input_params_base, public jsons_bytes_datagram_test, public input_params_jsons_2_action{
+  const deque<json_event>the_list;
+  deque<json_event>::const_iterator the_list_iter;
+  deque<json_event>::const_iterator the_list_end;
   // string for the ostringstream, in order to reserve size
   string ioss;
   ostringstream info_out_str_stream;  
 public:
-  input_params_atposs_hardcoded(void)=delete;
-  input_params_atposs_hardcoded(ostream&);
+  input_params_jsons_hardcoded(void)=delete;
+  input_params_jsons_hardcoded(ostream&);
   void import_next_event(vector<signals_param_action>&actions);
   unsigned long check_next_time_stamp(void);
   bool eot(void) const;
